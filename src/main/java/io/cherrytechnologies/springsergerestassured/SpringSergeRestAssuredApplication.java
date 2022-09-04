9720415c17753a1a998f40e14d8cba9fbc003d0f
rajan baliyan <rajansinghbaliyan@gmail.com>
2022-09-04 17:14:43 +0530

    initial commit

---
+++ src/main/java/io/cherrytechnologies/springsergerestassured/SpringSergeRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package io.cherrytechnologies.springsergerestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSergeRestAssuredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSergeRestAssuredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/cherrytechnologies/springsergerestassured/SpringSergeRestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cherrytechnologies.springsergerestassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSergeRestAssuredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
